e73a9b2ee0ed2d39f85d646080cc796f3403d24f
Logan Nochez <logannochez@hotmail.fr>
2021-07-01 16:02:37 +0200

    intégration

---
+++ js/index.js
@@ -0,0 +1,47 @@
+console.log("ça marche")
+
+let url = "bdd/FishEyeData.json";
+
+fetch(url)
+.then( (datas) =>{
+    return datas.json()
+})
+.then( (datas) => {
+
+    console.log(datas)
+
+    let photographes = datas.photographers
+
+    console.log(photographes)
+
+    let myHTML = ''
+    photographes.forEach(element => {
+        console.log(element.name)
+        myHTML += `<article>
+        <a href="photographers.html">
+          <img src="photos/Photographers ID Photos/${element.portrait}" alt="" />
+          <h2 class="name">${element.name}</h2>
+        </a>
+        <p class="location">${element.city}, ${element.country}</p>
+        <p class="tagline">${element.tagline}</p>
+        <p class="price">${element.price}/jour</p>
+        <ul class="filter">${element.tags.map(tag =>
+            `<li data-filter="${tag}">#${tag}</li>`).join(" ")}
+        </ul>
+      </article>`
+
+      
+
+        //Je crée mon HTML pour l'intégrer a la page
+    
+    });
+    
+
+    console.log(myHTML)
+
+    document.getElementById('photographers').innerHTML = myHTML
+
+})
+.catch((err) =>{
+    console.error(err)
+});
