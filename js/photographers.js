f5eccd3607cca1d1c5367220e5050e91847665c2
Logan Nochez <logannochez@hotmail.fr>
2021-08-24 17:39:01 +0200

    lightbox

--- js/photographers.js
+++ js/photographers.js
@@ -111,52 +111,107 @@ new apiFishEye().getDataFishEye().then( (datas) => {
     document.getElementById('ph_works').innerHTML += template;
   }})
 
+  Lightbox.init();
+
   return this;
 })
 .catch((err) =>{
     console.error(err)
 });
 
-// let lightbox = document.getElementsByClassName('lightbox');
 
-// let ph_work_elt = document.querySelectorAll('.ph_work_elt');
-
-// ph_work_elt.forEach((a) => a.addEventListener("click", openLightbox));
-
-// function openLightbox() {
-//   lightbox.style.display = 'block';
-//   console.log('ca marche');
-// }
+/**
+ * @property {HTMLElement} element
+ * @property {string[]} images Chemins des images de la lightbox
+ * @property {string} url Image actuellement affichée
+ */
 
 class Lightbox {
 
   static init() {
-    const ph_medias = document.querySelectorAll('.ph_media_link')
-      .forEach(ph_media => ph_media.addEventListener('click', e =>
+    const ph_medias = Array.from(document.querySelectorAll('.ph_media_link'));
+    // console.log("hey" ,ph_medias)
+
+    const gallery = ph_medias.map(ph_media => ph_media.getAttribute('href'))
+    // debugger
+    ph_medias.forEach(ph_media => ph_media.addEventListener('click', e =>
       {
+        // console.log('click')
         e.preventDefault()
-        new Lightbox(e.currentTarget.getAttribute('href'))
+        new Lightbox(e.currentTarget.getAttribute('href'), gallery)
       }))
   }
 
   /** 
   *@param {string} url URL des medias
-  *@return {HTMLElement}
+  *@param {string[]} gallery Chemin des medias de la Lightbox
   */
 
-  constructor(url) {
+  constructor(url, gallery) {
+    // console.log(url)
     const element = this.buildDom(url);
+    // console.log(element)
+    this.gallery = gallery
+    this.onKeyUp = this.onKeyUp.bind(this);
     document.body.appendChild(element);
+    document.addEventListener('keyup', this.onKeyUp);
+  }
+  /**
+   * @param {string} url URL de l'image 
+   */
+
+  loadImage(url) {
+    this.url = null
+    const image = new Image()
+    const container = this.element.querySelector('.lightbox__container')
+    container.innerHTML = ``
+    container.appendChild(image)
+    this.url = url
+    image.src = url
+  }
+
+  /**
+   * @param {KeyboardEvent} e 
+   */
+
+  onKeyUp (e) {
+    if (e.key === 'Escape') {
+      this.close(e)
+    }
   }
 
   /** 
   * ferme la lightbox
-  *@param {MouseEvent} e
+  *@param {MouseEvent/KeyboardEvent} e
   */
 
   close(e) {
-    e.preventDefault();
-    this.element.parentElement.removeChild(this.element);
+    e.preventDefault()
+    const lightbox = document.querySelector('.lightbox')
+    lightbox.classList.add('fadeOut')
+    window.setTimeout(() => {
+      lightbox.parentElement.removeChild(lightbox)
+    }, 100)
+    document.removeEventListener('keyup', this.onKeyUp);
+  }
+
+  /** 
+  *@param {MouseEvent/KeyboardEvent} e
+  */
+
+  next(e) {
+    e.preventDefault()
+    let i = this.images.findIndex(image => image === this.url)
+    this.loadImage(this.images[i + 1])
+  }
+  
+  /** 
+  *@param {MouseEvent/KeyboardEvent} e
+  */
+
+  prev(e) {
+    e.preventDefault()
+
   }
 
   /** 
@@ -166,34 +221,38 @@ class Lightbox {
   buildDom(url) {
     const dom = document.createElement('div');
     dom.classList.add('lightbox');
-    dom.innerHTML = `<i class="arrow_left fas fa-chevron-left"></i>
+    dom.innerHTML = `<i class="lightbox__prev fas fa-chevron-left"></i>
 
-    <div>
+    <div class="lightbox__container">
 
-      <img src="photos/Mimi/Animals_Rainbow.jpg" alt="">
+      <img src="${url}" alt="">
       <h2 class="lightbox_title">Rainbow Bird</h2>
 
     </div>
     <i class="lightbox_close_icon fa fa-times"></i>
-    <i class="arrow_right fas fa-chevron-right"></i>`;
+    <i class="lightbox__next fas fa-chevron-right"></i>`;
     dom.querySelector('.lightbox_close_icon').addEventListener('click', this.close.bind(this));
+    dom.querySelector('.lightbox__next').addEventListener('click', this.next.bind(this));
+    dom.querySelector('.lightbox__prev').addEventListener('click', this.prev.bind(this));
     return dom;
   }
-
 }
 
-Lightbox.init()
 
 
 
-// (function dropDown() {
-//   let dropDownMenu = document.getElementById('sort-wrapper');
 
-//   dropDownMenu.addEventListener('click', event => {
-//     let dropDownMenuOpen = document.getElementsByClassName('sort-wrapper-open');
+function dropDown() {
+  let dropDownMenu = document.getElementById('sort-wrapper');
+  // console.log(dropDownMenu);
+  dropDownMenu.addEventListener('click', event => {
+    
+    let dropDownMenuOpen = document.getElementsByClassName('sort-wrapper-open');
+    // console.log(dropDownMenuOpen);
+    dropDownMenu.style.display = 'none';
 
-//     dropDownMenu.style.display = 'none';
+    dropDownMenuOpen[0].style.display = 'flex';
+  });
+};
 
-//     dropDownMenuOpen.style.display = 'display';
-//   });
-// });
+dropDown();
