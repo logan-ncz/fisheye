ab1c39176fdf1797f0d229ea794434e335de68a8
Logan Nochez <logannochez@hotmail.fr>
2021-09-21 18:45:36 +0200

    trier par terminé

--- js/photographers/dropDownSort.js
+++ js/photographers/dropDownSort.js
@@ -1,12 +1,12 @@
+import renderMedia from "./renderMedia.js";
+
 export default class dropDownMenu {
     dropDown(datas) {
         let dropDownMenu = document.getElementById('sort-wrapper');
-        // console.log(dropDownMenu);
         let dropDownMenuOpen = document.getElementsByClassName('sort-wrapper-open');
         
         dropDownMenu.addEventListener('click', event => {
             
-            // console.log(dropDownMenuOpen);
             dropDownMenu.style.display = 'none';
         
             dropDownMenuOpen[0].style.display = 'flex';
@@ -29,56 +29,66 @@ export default class dropDownMenu {
         let dropDownMenu = document.getElementById('sort-btn');
         let dropDownMenuOpen = document.getElementsByClassName('sort-wrapper-open');
         let sortBtn = Array.from(document.getElementsByClassName('sort-btn'));
-        console.log(sortBtn)
-
+        
         sortBtn.forEach((btn, index) => btn.addEventListener('click', () => {
             dropDownMenuOpen [0].style.display = "none";
             if (index == 0) {
                 dropDownMenu.innerHTML = `Popularité <i class="fas fa-chevron-down arrow-down-open"></i>`;
-
                 
+                mediaArraySort = media.sort((a, b) => { // Sort by Popularity 
+                    return b.likes - a.likes
+                })
 
             } else if (index == 1) {
                 dropDownMenu.innerHTML = `Date <i class="fas fa-chevron-down arrow-down-open"></i>`;
 
-                
+                mediaArraySort = media.sort((a, b) => { // Sort by Date
+                    return new Date(a.date).valueOf() - new Date(b.date).valueOf();
+                })
 
             } else if (index == 2) {
                 dropDownMenu.innerHTML = `Titre <i class="fas fa-chevron-down arrow-down-open"></i>`;
 
-                
+                mediaArraySort = media.sort((a, b) => { // Sort by Title
+                    if (a.title.toLowerCase() < b.title.toLowerCase()) {
+                        return -1;
+                    } else if (a.title.toLowerCase() > b.title.toLowerCase()) {
+                        return 1;
+                    }
+                })
             }
-            // this.displaySortMedia(mediaArraySort);
+            
+            document.getElementById('ph_works').innerHTML = ``
+            this.displaySortMedia(mediaArraySort);
         }));
     }
 
-    // displaySortMedia(mediaArraySort) {
-    //     mediaArraySort.forEach(element => {
-    //         const UrlParam = new URLSearchParams(window.location.search);
-    //         const id = UrlParam.get('id');
+    displaySortMedia(mediaArraySort) {
+        mediaArraySort.forEach(element => {
+            const UrlParam = new URLSearchParams(window.location.search);
+            const id = UrlParam.get('id');
 
-    //         if (id == element.photographerId) {
+            if (id == element.photographerId) {
                 
-    //           let MediaFactory = renderMedia(element);
-    //           let mediaHTML = MediaFactory.choiceElement();
-    //           // console.log(mediaHTML)
+              let MediaFactory = renderMedia(element);
+              let mediaHTML = MediaFactory.choiceElement();
         
-    //           let template = `<article class="ph_work_elt">
-    //           <a class="ph_media_link" href='${mediaHTML.src}' title=${element.title}>
-    //             ${mediaHTML.outerHTML}
-    //           </a>
-    //           <div class="ph_work_elt_text">
-    //             <h2 class="ph_work_title">${element.title}</h2>
-    //             <div class="ph_elt_like">
-    //               <span class="ph_work_like">${element.likes}</span>
-    //               <i class="far fa-heart"></i>
-    //             </div>
-    //           </div>
-    //             </article>`
+              let template = `<article class="ph_work_elt">
+              <a class="ph_media_link" href='${mediaHTML.src}' title=${element.title}>
+                ${mediaHTML.outerHTML}
+              </a>
+              <div class="ph_work_elt_text">
+                <h2 class="ph_work_title">${element.title}</h2>
+                <div class="ph_elt_like">
+                  <span class="ph_work_like">${element.likes}</span>
+                  <i class="far fa-heart"></i>
+                </div>
+              </div>
+                </article>`
         
-    //         document.getElementById('ph_works').innerHTML += template;
-    //         }})
-    // }
+            document.getElementById('ph_works').innerHTML += template;
+            }})
+    }
 
 }
 
